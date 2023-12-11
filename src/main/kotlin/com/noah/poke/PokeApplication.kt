411e65b255e0883f2877882d6fc5f6c075e459a0
dev-seokho <loyu78@gmail.com>
2023-12-11 19:24:02 +0900

    초기세팅

---
+++ src/main/kotlin/com/noah/poke/PokeApplication.kt
@@ -0,0 +1,11 @@
+package com.noah.poke
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokeApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokeApplication>(*args)
+}
---
+++ src/test/kotlin/com/noah/poke/PokeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.noah.poke
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PokeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
