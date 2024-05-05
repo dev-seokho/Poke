2b003c5c47f87681958883c6b0a00f969e519927
dev-seokho <loyu78@gmail.com>
2024-05-06 00:28:40 +0900

    스웨거에 인증 시스템 추가

--- src/main/kotlin/com/noah/sns/poke/global/configuration/SwaggerConfig.kt
+++ src/main/kotlin/com/noah/sns/poke/global/configuration/SwaggerConfig.kt
@@ -3,19 +3,33 @@ package com.noah.sns.poke.global.configuration
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 
 
 @Configuration
 class SwaggerConfig {
     @Bean
     fun openAPI(): OpenAPI = OpenAPI()
-        .components(Components())
+        .components(Components().addSecuritySchemes("Authorization", securitySchemaInfo()))
+        .addSecurityItem(securityRequirementInfo())
         .info(apiInfo())
 
     private fun apiInfo() = Info()
         .title("Poke API")
         .description("Poke REST API")
         .version("1.0.0")
+
+    private fun securitySchemaInfo(): SecurityScheme = SecurityScheme()
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("Authorization")
+        .`in`(SecurityScheme.In.HEADER)
+        .name(HttpHeaders.AUTHORIZATION)
+
+    private fun securityRequirementInfo(): SecurityRequirement = SecurityRequirement()
+        .addList("Authorization")
 }
